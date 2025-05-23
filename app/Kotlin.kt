1a264b3dbfea3b276c07a7bf63831e3553ecf52c
joycebila <Joycebila721@gmail.com>
2025-05-23 07:13:11 +0000

    add

---
+++ app/Kotlin.kt
@@ -0,0 +1,7 @@
+class Kotlin {
+}object WeatherData {
+    val cities = arrayOf("Toronto", "Vancouver", "Montreal")
+    val temperatures = arrayOf(20, 15, 10)
+    val conditions = arrayOf("Sunny", "Rainy", "Cloudy")
+}val conditions = arrayOf("Sunny", "Rainy", "Cloudy", "Snowy", "Stormy")
+
